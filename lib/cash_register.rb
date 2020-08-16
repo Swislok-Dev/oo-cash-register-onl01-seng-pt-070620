c4b0aba529a4c61bfcff57575f64e4981b3aa4c8
Cory Marker <cmarker4@gmail.com>
2020-08-16 22:07:36 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -3,7 +3,7 @@ require "pry"
 class CashRegister
   attr_accessor :total, :discount, :items, :price, :last_transaction
 
-  def initialize(discount = 0)
+  def initialize(discount = 20)
     @total = 0
     @discount = discount
     @items = []
